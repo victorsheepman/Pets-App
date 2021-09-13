b8e9381688d063db68bb3d09f2be073b3c3f790c
unknown <chirlychplin@gmail.com>
2021-09-13 16:29:23 -0400

    footer 1.3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/main.scss";
 import "./utils/loader";
 import router from "./routes/index";
-window.addEventListener('load', router);
+//window.addEventListener('load', router);
 
 window.addEventListener('hashchange', router);
